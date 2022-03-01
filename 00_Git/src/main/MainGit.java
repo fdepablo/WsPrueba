8ee1ec28bb362223152f8450f27eea7fa403ec21
Felix de Pablo <f.depablo.lobo@gmail.com>
2022-03-01 12:16:38 +0100

    lectura de numero por pantalla

--- 00_Git/src/main/MainGit.java
+++ 00_Git/src/main/MainGit.java
@@ -1,9 +1,14 @@
 package main;
 
+import java.util.Scanner;
+
 public class MainGit {
 
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo Java con Git :) :)");
+		Scanner sc = new Scanner(System.in);
+		int numero1 = sc.nextInt();
+		System.out.println("El numero introducido ha sido " + numero1);
 	}
 
 }
