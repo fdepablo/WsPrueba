bfb98f502a3c22477eaaeaf8df0a9282990f5aca
Felix de Pablo <f.depablo.lobo@gmail.com>
2022-03-02 09:56:57 +0100

    add funcion suma de argumentos variable

--- 00_Git/src/main/MainGit.java
+++ 00_Git/src/main/MainGit.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class MainGit {
 
-	public static void main(String[] args) {
+	public static void main(String... args) {
 		System.out.println("Hola Mundo Java con Git :) :)");
 		Scanner sc = new Scanner(System.in);
 		int numero1 = sc.nextInt();
@@ -15,6 +15,16 @@ public class MainGit {
 		
 		int resultado = Suma.sumar(numero1, numero2);
 		System.out.println("El resultado es: " + resultado);
+		
+		resultado = Suma.sumar(1,2,3);
+		System.out.println("El resultado es: " + resultado);
+		
+		resultado = Suma.sumar(1,2,3,4,5,6,7);
+		System.out.println("El resultado es: " + resultado);
+		
+		resultado = Suma.sumar();
+		System.out.println("El resultado es: " + resultado);
+		
 	}
 
 }
--- 00_Git/src/main/Suma.java
+++ 00_Git/src/main/Suma.java
@@ -5,4 +5,18 @@ public class Suma {
 	public static int sumar(int n1, int n2) {
 		return n1 + n2;
 	}
+	
+	//La firma de un objeto
+	//1- Nombre metodo
+	//2- Tipo de argumetos de entrada
+	//3- El numero de argumentos de entrada
+	public static int sumar(int... numeros) {
+		int valorAcumulado = 0;
+		for(int numero : numeros) {
+			valorAcumulado += numero;
+		}
+		return valorAcumulado;
+	}
+
+
 }
